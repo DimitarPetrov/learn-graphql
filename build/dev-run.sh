0fbdcaba67a208ef5deef00251ed9fe38ca287c8
i356426 <buldozer9993@gmail.com>
2020-06-23 17:20:35 +0300

    Hot reload on change

--- build/dev-run.sh
+++ build/dev-run.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-srcPath="cmd"
-pkgFile="main.go"
 app="gql-server"
-src="$srcPath/$app/$pkgFile"
 
 printf "\nStart running: %s\n" "$app"
 # Set all ENV vars for the server to run
-export $(grep -v '^#' build/.env | xargs) && time go run $src
+export $(grep -v '^#' build/.env | xargs)
+time /$GOPATH/bin/realize start run
+# This should unset all the ENV vars, just in case.
 printf "\nStopped running: %s\n\n" "$app"
